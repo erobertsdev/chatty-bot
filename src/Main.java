114240bfe00d527e8a9a204c0243928cde11021b
Elias Roberts <elias@eroberts.dev>
2022-10-23 12:32:49 -0600

    wow, what a smart bot

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Earl.");
+        System.out.println("I was created in 2023.");
+    }
+}
